9e55d00a9d17745028955c54173080c31b673091
nointh <ngthnoi00@gmail.com>
2022-10-13 10:02:57 +0700

    reformat trust host

--- clinik/settings.py
+++ clinik/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$z&7@9b$2yy#dsmqmkoseeuzr0(^csk2#7zzsb5t@tdu&d-(v^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://clinic-webapp-mng.herokuapp.com/']
+ALLOWED_HOSTS = ['linic-webapp-mng.herokuapp.com/']
 
 CSRF_TRUSTED_ORIGINS = ["https://clinic-webapp-mng.herokuapp.com/"]
 
